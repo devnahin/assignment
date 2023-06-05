4921b44efe52348c26d481336ba6cc95037c382e
devnahin <dev.nahin@gmail.com>
2023-06-05 21:03:16 +0600

    assignment migration

---
+++ database/migrations/2023_06_05_122102_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('price', 50);
+            $table->text('description');
+            $table->integer('quantity')->nullable(); //Modify the existing migration 
+            $table->timestamp('created_at')->userCurrent();
+            $table->timestamp('updated_at')->userCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
