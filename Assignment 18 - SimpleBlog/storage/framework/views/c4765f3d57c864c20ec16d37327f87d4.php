fefe488eff7bb876a85cddc41e8b2dacedf443dd
devnahin <105677983+devnahin@users.noreply.github.com>
2023-07-03 22:48:04 +0600

    Upload the files

---
+++ Assignment 18 - SimpleBlog/storage/framework/views/c4765f3d57c864c20ec16d37327f87d4.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('components.category_data', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/shahriashormee/Desktop/SimpleBlog/SimpleBlog/resources/views/pages/categories.blade.php ENDPATH**/ ?>
